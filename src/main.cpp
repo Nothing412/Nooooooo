e1fa2a1502333fd9f2bcf800717e82dc6e97810e
Nothing412 <77010318+Nothing412@users.noreply.github.com>
2023-06-27 20:57:29 +0530

    New Code AND SFML 2.6.0

--- src/main.cpp
+++ src/main.cpp
@@ -1,52 +1,24 @@
-#include <SFML\Graphics.hpp>
-#include <SFML\Window.hpp>
-#include <SFML\System.hpp>
-#include <SFML\Audio.hpp>
-#include <iostream>
-#include "Player.h"
-#include "Taskbar.h"
-#include "Axe.h"
+#include<SFML\Graphics.hpp>
+#include<SFML\Window.hpp>
+#include<SFML\System.hpp>
+#include<iostream>
+#include<Grid.h>
 using namespace sf;
 using namespace std;
 
-int main() {
-    RenderWindow window(sf::VideoMode(800, 800), "Cat Woodcutter", sf::Style::Fullscreen);
-    window.setFramerateLimit(60);
-    
-    Player player;
-    Taskbar taskbar;
-    View view(player.getPositionForView(),Vector2f(window.getSize().x,window.getSize().y));
-    AxeItem axe;
-    Clock clock;
-    
-    while (window.isOpen()) {
+int main(){
+    RenderWindow window(VideoMode(1920,1080),"Test");
+    RectangleShape rectangle;
+    rectangle.setSize(sf::Vector2f(1920, 1080));
+    while(window.isOpen()){
         Event event;
-        while (window.pollEvent(event)) {
-            if (event.type == Event::Closed) {
+        while(window.pollEvent(event)){
+            if(event.type == Event::Closed){
                 window.close();
             }
-        }
-        
-        player.Update(clock);
-        taskbar.Update();
-        
-        clock.restart();
-        
+        }   
         window.clear();
-        
-        window.setView(view);
-        view.setCenter(Vector2f(player.getPositionForView().x,player.getPositionForView().y));
-        
-        axe.Draw(window);
-        
-        player.draw(window);
-        
-        window.setView(window.getDefaultView());
-        
-        taskbar.Draw(window);
-        
+        window.draw(rectangle);
         window.display();
     }
-    
-    return 0;
 }
